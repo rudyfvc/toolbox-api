f1845b35b37018bd81b9912bc6f8a87b3b23ec71
rfvasquez <rfvasquez@tigo.com.gt>
2023-08-10 11:51:31 -0600

    fix cors

--- src/index.js
+++ src/index.js
@@ -4,22 +4,20 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(require('./routes/files'));
-
-app.use(cors({
-    origin: "http://localhost:5173/"
-}));
-
-
-/*
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization, AuthorizationReporter"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
     next();
-});
-*/
+  });
+
+app.use(require('./routes/files'));
 
 app.listen(app.get('port'), () => {
     console.log(`Server listening on port ${app.get('port')}`);
